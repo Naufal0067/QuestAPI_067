bfb5758354d87e578098ecb75eac9bd494f02848
Naufal0067 <naufalopang1928@gmail.com>
2025-12-18 21:32:37 +0700

    menambahkan code untuk peta navigasi

---
+++ app/src/main/java/com/example/localrestapi/uicontroller/PetaNavigasi.kt
@@ -0,0 +1,37 @@
+package com.example.localrestapi.uicontroller
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.localrestapi.uicontroller.route.DestinasiEntry
+import com.example.localrestapi.uicontroller.route.DestinasiHome
+import com.example.localrestapi.view.EntrySiswaScreen
+import com.example.localrestapi.view.HomeScreen
+
+
+@Composable
+fun DataSiswaApp(navController: NavHostController = rememberNavController(), modifier: Modifier){
+    HostNavigasi(navController = navController)
+}
+
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+){
+    NavHost(navController = navController, startDestination = DestinasiHome.route, modifier = Modifier ) {
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+                navigateToItemUpdate = {
+//                    navController.navigate("${DestinasiDetail.route}/${it}")
+                })
+        }
+        composable(DestinasiEntry.route) {
+            EntrySiswaScreen(navigateBack = { navController.navigate(DestinasiHome.route) })
+        }
+    }
+}
