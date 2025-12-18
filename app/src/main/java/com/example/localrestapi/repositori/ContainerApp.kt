9ad527e44d8da5d819ab85f0e9d496231520d395
Naufal0067 <naufalopang1928@gmail.com>
2025-12-18 12:19:20 +0700

    menambahkan class dan mengkonfigurasi http

--- app/src/main/java/com/example/localrestapi/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/localrestapi/repositori/ContainerApp.kt
@@ -1,6 +1,20 @@
 package com.example.localrestapi.repositori
 
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+
 interface ContainerApp {
     val repositoryDataSiswa : RepositoryDataSiswa
 }
+
+class DefaultContainerApp : ContainerApp{
+    private val baseurl = "http://10.0.2.2/umyTI/"
+
+    val logging = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
 
