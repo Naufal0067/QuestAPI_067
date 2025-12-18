cab3270b2211941849140e73ee1fc4d528ec0e10
Naufal0067 <naufalopang1928@gmail.com>
2025-12-18 12:41:39 +0700

    menambahkan class Entry View Model

---
+++ app/src/main/java/com/example/localrestapi/viewmodel/EntryViewModel.kt
@@ -0,0 +1,23 @@
+package com.example.localrestapi.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.localrestapi.modedata.DetailSiswa
+import com.example.localrestapi.modedata.UIStateSiswa
+import com.example.localrestapi.repositori.RepositoryDataSiswa
+
+class EntryViewModel(private val repositoryDataSiswa: RepositoryDataSiswa):
+        ViewModel() {
+            var uiStateSiswa by mutableStateOf(UIStateSiswa())
+                private set
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa ):
+            Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
+    fun updateUiState
+        }
