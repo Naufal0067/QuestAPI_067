9f6ab7521dcf29407f8df989540759cd10cf68d2
Naufal0067 <naufalopang1928@gmail.com>
2025-12-25 10:53:36 +0700

    menambahkan kolom dan padding di HalamanDetail

--- app/src/main/java/com/example/localrestapi/view/HalamanDetail.kt
+++ app/src/main/java/com/example/localrestapi/view/HalamanDetail.kt
@@ -61,3 +61,7 @@ private fun BodyDetailDataSiswa(
     onDelete: () -> Unit,
     modifier: Modifier = Modifier
 ) {
+    Column(
+        modifier = modifier.padding(dimensionResource(id = R.dimen.padding_medium)),
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
+    )
