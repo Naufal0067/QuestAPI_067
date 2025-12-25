3e01efb75b88a798fbe54c0a49c6704621b0ac9c
Naufal0067 <naufalopang1928@gmail.com>
2025-12-25 19:30:53 +0700

    menambahkan code untuk navigasi DestinasiEdit dan DestinasiDetail

--- app/src/main/java/com/example/localrestapi/uicontroller/route/DestinasiDetail.kt
+++ app/src/main/java/com/example/localrestapi/uicontroller/route/DestinasiDetail.kt
@@ -1,2 +1,10 @@
 package com.example.localrestapi.uicontroller.route
 
+import com.example.localrestapi.R
+
+object DestinasiDetail : DestinasiNavigasi {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
--- app/src/main/java/com/example/localrestapi/uicontroller/route/DestinasiEdit.kt
+++ app/src/main/java/com/example/localrestapi/uicontroller/route/DestinasiEdit.kt
@@ -1,2 +1,12 @@
 package com.example.localrestapi.uicontroller.route
 
+import com.example.localrestapi.R
+
+object DestinasiEdit : DestinasiNavigasi {
+    override val route = "edit_siswa"
+
+    override val titleRes = R.string.edit_siswa
+
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
