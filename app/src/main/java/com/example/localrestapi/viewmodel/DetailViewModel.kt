1db94daf457ecc296dd73c1b4f2ae61d49902fb7
Naufal0067 <naufalopang1928@gmail.com>
2025-12-25 12:30:38 +0700

    menambahkan sealed interface StatusUIDetail

--- app/src/main/java/com/example/localrestapi/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/localrestapi/viewmodel/DetailViewModel.kt
@@ -1,8 +1,18 @@
 package com.example.localrestapi.viewmodel
 
+import android.annotation.SuppressLint
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.localrestapi.modedata.DataSiswa
 import com.example.localrestapi.repositori.RepositoryDataSiswa
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import retrofit2.Response
+import java.io.IOException
 
 sealed interface StatusUIDetail {
     data class Success(val satusiswa: DataSiswa) : StatusUIDetail
