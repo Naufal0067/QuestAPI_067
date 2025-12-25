5cfdf0e66e92760628021fd3593d8d2d73a0d8ce
Naufal0067 <naufalopang1928@gmail.com>
2025-12-25 11:26:22 +0700

    membuat object PenyediaViewModel untuk detail view model dan edit view model

--- app/src/main/java/com/example/localrestapi/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/localrestapi/viewmodel/provider/PenyediaViewModel.kt
@@ -1,10 +1,13 @@
 package com.example.localrestapi.viewmodel.provider
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.localrestapi.repositori.AplikasiDataSiswa
+import com.example.localrestapi.viewmodel.DetailViewModel
+import com.example.localrestapi.viewmodel.EditViewModel
 import com.example.localrestapi.viewmodel.EntryViewModel
 import com.example.localrestapi.viewmodel.HomeViewModel
 import okhttp3.WebSocket
@@ -16,5 +19,15 @@ object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer { HomeViewModel(aplikasiDataSiswa().container.repositoryDataSiswa) }
         initializer { EntryViewModel(aplikasiDataSiswa().container.repositoryDataSiswa) }
+        initializer {
+            DetailViewModel(
+                this.createSavedStateHandle(),
+                aplikasiDataSiswa().container.repositoryDataSiswa) }
+        initializer {
+            EditViewModel(
+                this.createSavedStateHandle(),
+                aplikasiDataSiswa().container.repositoryDataSiswa
+            )
+        }
     }
 }
