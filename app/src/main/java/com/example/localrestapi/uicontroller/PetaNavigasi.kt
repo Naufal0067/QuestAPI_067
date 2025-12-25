3aaadad9efde7d451f323800b1f46d2f6f73bc99
Naufal0067 <naufalopang1928@gmail.com>
2025-12-25 11:20:54 +0700

    menambahkan code untuk navigasi DestinasiEdit

--- app/src/main/java/com/example/localrestapi/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/localrestapi/uicontroller/PetaNavigasi.kt
@@ -44,6 +44,16 @@ fun HostNavigasi(
                 navigateBack = { navController.navigate(DestinasiHome.route) }
             )
         }
-
+        composable(
+            DestinasiEdit.routeWithArgs,
+            arguments = listOf(navArgument(DestinasiEdit.itemIdArg) {
+                type = NavType.IntType
+            })
+        ) {
+            EditSiswaScreen(
+                navigateBack = { navController.navigate(DestinasiHome.route) },
+                onNavigateUp = { navController.navigateUp() }
+            )
+        }
     }
 }
